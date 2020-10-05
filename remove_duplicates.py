0c490cd64a9523585ce428df25cf254ef6347e7f
s0r3 <ggoodson2432@gmail.com>
2020-10-05 09:55:38 -0400

    Updated remove_duplicates.py
    made it work with python 3 since python 2 is deprecated

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -31,47 +31,48 @@ SOURCES:
 
 import sys
 import os
-import md5
+import _md5 as md5
 
 user_name = os.getlogin()
 music_dir = "/Users/" + user_name + "/Music/iTunes/iTunes Media/Music"
 cont = False
 
 print("This utility is to remove duplicate music from your iTunes\n"
-    + "library, though it will remove duplicates from any folder\n"
-    + "that you enter.\n")
+      + "library, though it will remove duplicates from any folder\n"
+      + "that you enter.\n")
 print("The default folder for iTunes is located at:\n"
-       + "\t" + music_dir)
+      + "\t" + music_dir)
 
 while (not cont):
-   temp = raw_input("\nEnter the name of your folder to remove duplicates\n"
-                  + "from or hit enter to use the suggested location\n> ")
-   
-   if (not temp):
-      temp = music_dir
+    temp = input("\nEnter the name of your folder to remove duplicates\n"
+                     + "from or hit enter to use the suggested location\n> ")
 
-   check = raw_input("\nIs this correct? (y/n)\n\t" + temp + "\n> ")
-   
-   if (check == 'y'):
-      music_dir = temp
-      cont = True
+    if (not temp):
+        temp = music_dir
+
+    check = input("\nIs this correct? (y/n)\n\t" + temp + "\n> ")
+
+    if (check == 'y'):
+        music_dir = temp
+        cont = True
 
 def remove_duplicates(dir):
-   unique = []
-   for filename in os.listdir(dir):
-      filename = dir + '/' + filename
-      if os.path.isfile(filename):
-         filehash = md5.md5(file(filename).read()).hexdigest()
-         if filehash not in unique: 
-            unique.append(filehash)
-         else:
-            print "Removing " + filename + "..."
-            os.remove(filename)
-      elif os.path.isdir(filename):
-         remove_duplicates(filename)
-      else:
-         print("Unable to find file/directory " + filename
-             + ", please try again")
+    unique = []
+    for filename in os.listdir(dir):
+        filename = dir + '/' + filename
+        if os.path.isfile(filename):
+            with open(filename, 'rb') as file:
+                filehash = md5.md5(file.read()).hexdigest()
+            if filehash not in unique:
+                unique.append(filehash)
+            else:
+                print("Removing " + filename + "...")
+                os.remove(filename)
+        elif os.path.isdir(filename):
+            remove_duplicates(filename)
+        else:
+            print("Unable to find file/directory " + filename + ", please try again")
 
-print "\nIt's working I promise, give it a minute or two...\n"
+
+print("\nIt's working I promise, give it a minute or two...\n")
 remove_duplicates(music_dir)
